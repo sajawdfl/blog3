8b0fd0ef4d3997a27f43449e3702ccbdfbfaa9d8
sajawdfl <sajawdfl@gmai.com>
2023-04-07 19:59:39 +0430

    phase1

---
+++ blog/models.py
@@ -0,0 +1,20 @@
+
+from django.conf import settings
+from django.db import models
+from jdatetime import datetime as dt
+
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=dt.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = dt.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+# Create your models here.
